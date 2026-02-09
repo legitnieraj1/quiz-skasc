a02a74886a0d38dc42a36e64fc1f43514c2508a9
nieraj niketan <nierajniketans25bcg063@skasc.ac.in>
2026-02-09 11:27:57 +0530

    Remove About navbar completely

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/ui/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         {children}
         <div className="fixed bottom-4 w-full text-center z-50 pointer-events-none mix-blend-difference">
           <div className="flex flex-col gap-1 items-center justify-center">
